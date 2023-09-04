25f7def57b97eaab785be1e2f655f3a8f43a7f0f
Albert Yumnam <84399334+Albx68@users.noreply.github.com>
2023-09-05 01:26:50 +0530

    added responsive grid

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,19 +2,24 @@
 
 import { COL_COUNT, ROW_COUNT } from '@/utils/constants/constants'
 import { generateGrid } from '@/utils/helpers/grid'
+import useWindowDimensions from '@/utils/hooks/useWindowDimensions'
+import { motion } from 'framer-motion'
 import React, { useMemo } from 'react'
 
 
 
 const Grid = () => {
+    const { height: windowHeight, width: windowWidth } = useWindowDimensions()
 
     const grid = useMemo(() => generateGrid(ROW_COUNT, COL_COUNT), [])
-
-    return <div>
+    const dimension = windowHeight * windowWidth
+    const totalNodes = ROW_COUNT * COL_COUNT
+    const unitNodeArea = dimension / totalNodes
+    const unitNode = Math.round(Math.sqrt(unitNodeArea))
+    return <div className='flex flex-wrap justify-center items-center'>
         {grid.map((row, rowIdx) => {
-            console.log("row", row)
             return row.map((col, colIdx) => {
-                return <div key={`${rowIdx}x${colIdx}`}>{rowIdx}x{colIdx}</div>
+                return <motion.div style={{ height: unitNode, width: unitNode }} key={`${rowIdx}x${colIdx}`} className={`text-xs text-emerald-700 flex justify-center items-center bg-emerald-50 border-2 border-emerald-700`}></motion.div>
             })
         })}
     </div>
